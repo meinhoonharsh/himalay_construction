dc6c9a5d600c2602f2cee6c542e0872113355fc6
meinhoonharsh <harshprogrammer782@gmail.com>
2025-01-30 19:41:07 +0530

    Created Draft build release

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,54 @@
+
+    <!-- Header Start -->
+	<header class="main-header">
+		<div class="header-sticky">
+			<nav class="navbar navbar-expand-lg">
+				<div class="container-fluid">
+					<!-- Logo Start -->
+					<a class="navbar-brand" href="#">
+						<img src="/builtup/images/logo.svg" alt="Logo">
+					</a>
+					<!-- Logo End -->
+
+					<!-- Main Menu Start -->
+					<div class="collapse navbar-collapse main-menu">
+                        <div class="nav-menu-wrapper">
+                            <ul class="navbar-nav mr-auto" id="menu">
+                                <li class="nav-item submenu"><a class="nav-link" href="#">Home</a>
+                                    <ul class="sub-menu">
+                                        <li class="nav-item"><a class="nav-link" href="#">Home - Image</a></li>
+                                        <li class="nav-item"><a class="nav-link" href="#">Home - Slider</a></li>
+                                        <li class="nav-item"><a class="nav-link" href="#">Home - Video</a></li>
+                                    </ul>
+                                </li>                                
+                                <li class="nav-item"><a class="nav-link" href="#">About Us</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#">Services</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#">Blog</a></li>
+                                <li class="nav-item submenu"><a class="nav-link" href="#">Pages</a>
+                                    <ul>                                        
+                                        <li class="nav-item"><a class="nav-link" href="#">Service Details</a></li>
+                                        <li class="nav-item"><a class="nav-link" href="#">Blog Details</a></li>                                       
+                                        <li class="nav-item"><a class="nav-link" href="#">Project</a></li>
+                                        <li class="nav-item"><a class="nav-link" href="#">Project Details</a></li>
+                                        <li class="nav-item"><a class="nav-link" href="#">Our Team</a></li>
+                                        <li class="nav-item"><a class="nav-link" href="#">FAQ</a></li>
+                                        <li class="nav-item"><a class="nav-link" href="#">404</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item highlighted-menu"><a class="nav-link" href="#">Contact Us</a></li>                               
+                            </ul>
+                        </div>
+                        <!-- Let’s Start Button Start -->
+                        <div class="header-btn d-inline-flex">
+                            <a href="#" class="btn-default">Contact Us</a>
+                        </div>
+                        <!-- Let’s Start Button End -->
+					</div>
+					<!-- Main Menu End -->
+					<div class="navbar-toggle"></div>
+				</div>
+			</nav>
+			<div class="responsive-menu"></div>
+		</div>
+	</header>
+	<!-- Header End -->
