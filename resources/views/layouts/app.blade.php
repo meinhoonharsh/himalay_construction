3ad9ae87fc4b5da4aa00ab98c63c5bf2324eecbd
meinhoonharsh <harshprogrammer782@gmail.com>
2025-02-13 23:56:38 +0530

    Updated Logo and Color Code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,7 +41,7 @@
 	<div class="preloader">
 		<div class="loading-container">
 			<div class="loading"></div>
-			<div id="loading-icon"><img src="/builtup/images/loader.svg" alt=""></div>
+			<div id="loading-icon"><img src="/builtup/images/loader.png" alt=""></div>
 		</div>
 	</div>
 	<!-- Preloader End -->
