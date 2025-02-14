d947d36e1013cf9be2e4eee90b174765040318f2
meinhoonharsh <harshprogrammer782@gmail.com>
2025-02-14 09:52:57 +0530

    Created Separate Footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,113 @@
+<!-- Footer Start -->
+<footer class="main-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-12">
+                <!-- About Footer Start -->
+                <div class="about-footer">
+                    <!-- Footer Logo Start -->
+                    <div class="footer-logo">
+                        <figure>
+                            <img src="/builtup/images/logo.png" alt="" style="height: 55px;">
+                        </figure>
+                    </div>
+                    <!-- Footer Logo End -->
+
+                    <!-- Footer Content Start -->
+                    <div class="footer-content">
+                        <p>
+                            At HouseBhopal, we specialize in transforming your visions into reality through exceptional craftsmanship and meticulous attention to detail.
+                        </p>
+                    </div>
+                    <!-- Footer Content End -->
+                </div>
+
+                <!-- About Footer End -->
+            </div>
+            <div class="col-md-1"></div>
+            <div class="col-md-1"></div>
+
+            <div class="col-md-3 col-12">
+                <!-- Footer Links Start -->
+                <div class="footer-links">
+                    <h3>company</h3>
+                    <ul>
+                        <li><a href="#about">about us</a></li>
+                        <li><a href="#services">services</a></li>
+                        <li><a href="/projects">projects</a></li>
+                        <li><a href="/faqs">faqs</a></li>
+                        <li><a href="/contact">contact us</a></li>
+                    </ul>
+                </div>
+                <!-- Footer Links End -->
+            </div>
+
+            <div class="col-md-3 col-12">
+                <!-- Footer Contact Info Box Start -->
+                <div class="footer-links footer-contact-box">
+                    <h3>contact us</h3>
+                    <!-- Info Box Start -->
+                    <div class="footer-info-box">
+                        <!-- Icon Box Start -->
+                        <div class="icon-box">
+                            <img src="/builtup/images/icon-phone.svg" alt="">
+                        </div>
+                        <!-- Icon Box End -->
+                        <p>+91 8770377846</p>
+                    </div>
+                    <!-- Info Box End -->
+
+                    <!-- Info Box Start -->
+                    <div class="footer-info-box">
+                        <!-- Icon Box Start -->
+                        <div class="icon-box">
+                            <img src="/builtup/images/icon-mail.svg" alt="">
+                        </div>
+                        <!-- Icon Box End -->
+                        <p>hi@housebhopal.com</p>
+                    </div>
+                    <!-- Info Box End -->
+
+                    <!-- Info Box Start -->
+                    <div class="footer-info-box">
+                        <!-- Icon Box Start -->
+                        <div class="icon-box">
+                            <img src="/builtup/images/icon-location.svg" alt="">
+                        </div>
+                        <!-- Icon Box End -->
+                        <p>37, Aradhana Nagar, Berkhera, BHEL, Bhopal (462022) </p>
+                    </div>
+                    <!-- Info Box End -->
+                </div>
+                <!-- Footer Contact Info Box End -->
+            </div>
+        </div>
+
+        <!-- Footer Copyright Section Start -->
+        <div class="footer-copyright">
+            <div class="row align-items-center">
+                <div class="col-lg-6 col-md-7">
+                    <!-- Footer Copyright Start -->
+                    <div class="footer-copyright-text">
+                        <p>Copyright © 2017 HouseBhopal. All Rights Reserved.</p>
+                    </div>
+                    <!-- Footer Copyright End -->
+                </div>
+
+                <div class="col-lg-6 col-md-5">
+                    <!-- Footer Social Link Start -->
+                    <div class="footer-social-links">
+                        <ul>
+                            <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+                            <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
+                            <li><a href="#"><i class="fa-brands fa-linkedin-in"></i></a></li>
+                        </ul>
+                    </div>
+                    <!-- Footer Social Link End -->
+                </div>
+            </div>
+        </div>
+        <!-- Footer Copyright Section End -->
+    </div>
+</footer>
+<!-- Footer End -->
